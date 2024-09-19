7a0afbf519c8321814a18dfd80c8b06000d72d27
Yu-jiin <jiin4083@naver.com>
2024-09-19 09:55:11 +0900

    중간저장

--- 02-template/articles/views.py
+++ 02-template/articles/views.py
@@ -1,13 +1,23 @@
-# import random
+import random
 from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'articles/index.html')
+    context = {
+        'name' : 'alice' ,
+        'ssafy' : {
+            'key' : 'value'
+        }
+    }
+    return render(request, 'articles/index.html', context)
 
 
 def dinner(request):
-    # foods = ['국밥', '국수', '카레', '탕수육',]
-    # picked = random.choice(foods)
-    pass
+    foods = ['국밥', '국수', '카레', '탕수육',]
+    picked = random.choice(foods)
+    context = {
+        'foods' : foods,
+        'picked' : picked,
+    }
+    return render(request, 'articles/dinner.html', context)
