670e89fb852227f557ada73a188e4e86f9def2b9
Yujiin <jiin4083@naver.com>
2024-09-23 10:44:40 +0900

    전체게시물 조회 및 보여주기

--- 04-orm/articles/views.py
+++ 04-orm/articles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+# 모델 클래스 가져와
+from .models import Article
 # Create your views here.
+def index(request):
+    # 전체 조회
+    articles = Article.objects.all()
+
+    context = {
+        'articles' : articles,
+    }
+
+    return render(request, 'articles/index.html', context)
--- 04-orm/crud/urls.py
+++ 04-orm/crud/urls.py
@@ -1,22 +1,27 @@
-"""
-URL configuration for crud project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('articles/', include('articles.urls')),
 ]
+
+# url app으로 나누기
+    # 1. include 추가 
+    # 2. path('articles/', include('articles.urls')), 추가
+    # 3. articles 밑에 urls.py 만들기 
+    # 4. 기존 url에서 긁어와서 필요없는거 지우고 바꾸기 
+        # from django.urls import path
+        # from . import views
+
+        # app_name = 'articles'
+        # urlpatterns = [
+        #     path('', views.index, name='index'),
+        # ]
+
+    # 후에 views에 index 함수 작성 
+    # def index(request):
+    #     return render(request, 'articles/index.html')
+
+    # 5. articles 밑에 templates/articles/index.html 생성
