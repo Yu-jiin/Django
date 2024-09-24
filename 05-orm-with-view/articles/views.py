531b880c3d67b7e774c1d25e1d863da444f9be13
Yujiin <jiin4083@naver.com>
2024-09-24 09:40:07 +0900

    게시물 생성 done

--- 05-orm-with-view/articles/views.py
+++ 05-orm-with-view/articles/views.py
@@ -18,3 +18,32 @@ def detail(request, pk):
         'article' : article,
     }
     return render(request, 'articles/detail.html', context)
+
+def new(request):
+    # 게시글 작성 페이지 응답
+    return render(request, 'articles/new.html')
+
+def create(request):
+    # 1. 사용자 요청으로부터 입력 데이터 추출
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+
+    # 1번과 2번이 동일하다만, 1번은 너무 길기에 2번 사용
+    # # 저장 1
+    # article = Article()
+    # article.title = title
+    # article.content = content
+    # article.save()
+
+    # 저장 2
+    article = Article(title = title, content = content)
+    article.save()
+
+
+    # 유효성 검사에서 탈락 쓰지마 
+    # # 저장 3
+    # Article.objects.create(title = title, content = content)
+
+    # 2. 추출한 입력데이터를 활용해 DB에 저장 요청
+
+    return render(request, 'articles/create.html')
