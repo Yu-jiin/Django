166e2fef9b74e17abef946b73505e9062d32ad00
Yujiin <jiin4083@naver.com>
2024-09-25 10:59:56 +0900

    create + new, update + edit 합치기 done

--- 06-form/articles/forms.py
+++ 06-form/articles/forms.py
@@ -8,8 +8,35 @@ from .models import Article
 #     content = forms.CharField(widget=forms.Textarea)
 
 
+# class ArticleForm(forms.ModelForm):
+#     class Meta:
+#         model = Article
+#         fields = '__all__'
+#         # exclude = ('title',)
+
 class ArticleForm(forms.ModelForm):
+    title = forms.CharField(
+        label='제목',
+        widget=forms.TextInput(
+            attrs={
+                'class' : 'my-title',
+                'placdholder' : 'Enter the title',
+                'maxlength' : 10,
+            }
+        ),
+    )
+    content = forms.CharField(
+        label='내용',
+        widget=forms.Textarea(
+            attrs={
+                'class' : 'my-content',
+                'placeholder' : 'Enter the content',
+                'rows' : 5,
+                'cols' : 50,
+            }
+        ),
+        error_messages={'required' : '내용을 입력해주세요'}
+    )
     class Meta:
         model = Article
         fields = '__all__'
-        # exclude = ('title',)
--- 06-form/articles/views.py
+++ 06-form/articles/views.py
@@ -23,34 +23,48 @@ def detail(request, pk):
     return render(request, 'articles/detail.html', context)
 
 
-def new(request):
-    form = ArticleForm()
-    context = {
-        'form': form,
-    }
-    # 게시글 작성 페이지 응답
-    return render(request, 'articles/new.html', context)
+# def new(request):
+#     form = ArticleForm()
+#     context = {
+#         'form': form,
+#     }
+#     # 게시글 작성 페이지 응답
+#     return render(request, 'articles/new.html', context)
 
 
-def create(request):
-    # title = request.POST.get('title')
-    # content = request.POST.get('content')
-    # article = Article(title=title, content=content)
-    # article.save()
+# def create(request):
+#     # title = request.POST.get('title')
+#     # content = request.POST.get('content')
+#     # article = Article(title=title, content=content)
+#     # article.save()
 
-    # 모델 폼 인스턴스 생성(사용자 입력 데이터 통째)
-    form = ArticleForm(request.POST)
-    # 유효성 검사
-    if form.is_valid():     # -> return 값 = T or F 
-        article = form.save()
-        return redirect('articles:detail', article.pk)
+#     # 모델 폼 인스턴스 생성(사용자 입력 데이터 통째)
+#     form = ArticleForm(request.POST)
+#     # 유효성 검사
+#     if form.is_valid():     # -> return 값 = T or F 
+#         article = form.save()
+#         return redirect('articles:detail', article.pk)
     
+#     context = {
+#         'form' : form,
+#     }
+#     return render(request, 'articles/new.html', context)
+    
+# new + create 결합
+def create(request):
+    # 요청 메서드가 POST 일 때
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', article.pk)
+    # 요청 메서드가 POST가 아닐 때 GET PUT DELETE
+    else:
+        form = ArticleForm()
     context = {
         'form' : form,
     }
-    return render(request, 'articles/new.html', context)
-    
-
+    return render(request, 'articles/create.html', context)
 
 
 
@@ -63,26 +77,52 @@ def delete(request, pk):
     return redirect('articles:index')
 
 
-def edit(request, pk):
-    # 어떤 게시글을 수정할지 조회
-    article = Article.objects.get(pk=pk)
-    context = {
-        'article': article,
-    }
-    return render(request, 'articles/edit.html', context)
+# def edit(request, pk):
+#     # 어떤 게시글을 수정할지 조회
+#     article = Article.objects.get(pk=pk)
+#     form = ArticleForm(instance=article)
+#     context = {
+#         'article': article,
+#         'form' : form,
+#     }
+#     return render(request, 'articles/edit.html', context)
+
+
+# def update(request, pk):
+#     # 1. 어떤 게시글 수정할지 조회
+#     article = Article.objects.get(pk=pk)
+#     # 2. 사용자로부터 받은 새로운 입력 데이터 추출
+#     # title = request.POST.get('title')
+#     # content = request.POST.get('content')
+#     # # 3. 기존 게시글의 데이터를 사용자로 받은 데이터로 새로 할당
+#     # article.title = title
+#     # article.content = content
+#     # # 4. 저장
+#     # article.save()
+
+#     form = ArticleForm(request.POST, instance=article)
+#     if form.is_valid():
+#         form.save()
+#         return redirect('articles:detail', article.pk)
+#     context = {
+#         'article' : article,
+#         'form' : form,
+#     }
+#     return render(request, 'articles/edit.html', context)
 
 
 def update(request, pk):
-    # 1. 어떤 게시글 수정할지 조회
     article = Article.objects.get(pk=pk)
-    # 2. 사용자로부터 받은 새로운 입력 데이터 추출
-    title = request.POST.get('title')
-    content = request.POST.get('content')
-    # 3. 기존 게시글의 데이터를 사용자로 받은 데이터로 새로 할당
-    article.title = title
-    article.content = content
-    # 4. 저장
-    article.save()
-
-    return redirect('articles:detail', article.pk)
-
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        # article = Article.objects.get(pk=pk)
+        form = ArticleForm(instance=article)
+    context = {
+        'article' : article,
+        'form' : form,
+    }
+    return render(request, 'articles/update.html', context)
