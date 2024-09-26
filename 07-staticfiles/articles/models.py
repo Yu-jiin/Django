b99030643c6d248eb75a5184441a40b21e7a423b
Yujiin <jiin4083@naver.com>
2024-09-26 11:41:46 +0900

    image done

--- 07-staticfiles/articles/models.py
+++ 07-staticfiles/articles/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=10)
     content = models.TextField()
-    image = models.ImageField(blank=True)
+    image = models.ImageField(blank=True, upload_to='%Y/%m/%d/')
     # blank=True 속성을 작성하면 빈 문자열도 작성 가능 사진 안올리는 사람 경우 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
