a81d34d4bf84117761bdb3d9a7eb96fcaa994f19
Yujiin <jiin4083@naver.com>
2024-09-26 10:07:40 +0900

    image

--- 07-staticfiles/articles/models.py
+++ 07-staticfiles/articles/models.py
@@ -4,5 +4,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=10)
     content = models.TextField()
+    image = models.ImageField(blank=True)
+    # blank=True 속성을 작성하면 빈 문자열도 작성 가능 사진 안올리는 사람 경우 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- 07-staticfiles/crud/urls.py
+++ 07-staticfiles/crud/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# static(URL주소, 미디어 파일의 실제 위치)
