975d23a6d5dd0c4d1f9ecb12337464609a2e3d5e
Yujiin <jiin4083@naver.com>
2024-09-27 15:18:06 +0900

    CRUD done

--- project_movie/movies/views.py
+++ project_movie/movies/views.py
@@ -16,7 +16,7 @@ def create(request):
         form = MovieForm(request.POST, request.FILES)
         if form.is_valid():
             movie = form.save()
-            return redirect('movies:index')
+            return redirect('movies:detail', movie.pk)
     else:
         form = MovieForm()
     context = {
@@ -25,8 +25,28 @@ def create(request):
     return render(request, 'movies/create.html', context)
 
 def detail(request, pk):
-    movie = Movie().objects.get(pk=pk)
+    movie = Movie.objects.get(pk=pk)
     context = {
         'movie' : movie,
     }
     return render(request, 'movies/detail.html', context)
+
+def update(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = MovieForm(request.POST, request.FILES, instance=movie)
+        if form.is_valid():
+            movie = form.save()
+            return redirect('movies:detail', movie.pk)
+    else:
+        form = MovieForm(instance=movie)
+    context = {
+        'movie' : movie,
+        'form' : form,
+    }
+    return render(request, 'movies/update.html', context)
+
+def delete(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    movie.delete()
+    return redirect('movies:index')
