8e85f3d9c3019d3af755b5d7c096b71329cd692e
Yujiin <jiin4083@naver.com>
2024-09-27 16:40:51 +0900

    0927 done

--- project_movie/movies/models.py
+++ project_movie/movies/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=250)
     content = models.TextField()
-    images = models.ImageField(blank=True)
+    images = models.ImageField(blank=True, null=True)
