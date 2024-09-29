2f9d9aefe47134e8e2e5828d37603ac743fd694d
Yujiin <jiin4083@naver.com>
2024-09-29 13:47:56 +0900

    index.html 완료

--- my_pjt04/movies/models.py
+++ my_pjt04/movies/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=250)
-    content = models.TextField()
-    
+    description = models.TextField()
+    image = models.ImageField(blank=True)
