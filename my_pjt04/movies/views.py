ebbbb384c399ad7ec7ec42a141d8d9ed5520ef7c
Yujiin <jiin4083@naver.com>
2024-09-29 14:23:13 +0900

    CRUD 영화 등록, 수정, 제출, 삭제 all done

--- my_pjt04/movies/views.py
+++ my_pjt04/movies/views.py
@@ -35,3 +35,18 @@ def delete(request, pk):
     movie = Movie.objects.get(pk=pk)
     movie.delete()
     return redirect('movies:index')
+
+def update(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = MovieForm(request.POST, request.FILES, instance = movie)
+        if form.is_valid():
+            movie = form.save()
+            return redirect('movies:detail', movie.pk)
+    else:
+        form = MovieForm(instance = movie)
+    context = {
+        'form' : form,
+        'movie' : movie,
+    }
+    return render(request, 'movies/update.html', context)
