1f630011453b1bb76496fe8791c0033d2862660f
Yujiin <jiin4083@naver.com>
2024-10-02 09:17:31 +0900

    signup ing

--- 08-02-authentication/accounts/views.py
+++ 08-02-authentication/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 
@@ -22,3 +22,17 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return redirect('articles:index')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request, request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = UserCreationForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/signup.html', context)
