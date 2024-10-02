094e5407407a6b809d74b9274668f599972efc33
Yujiin <jiin4083@naver.com>
2024-10-02 09:27:18 +0900

    customuserChangeForm

---
+++ 08-02-authentication/accounts/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+# django는 User 모델 직접 참조를 권장 X 
+# from .models import User
+
+# 그래서 django는 User 간접참조 방법 제공 
+from django.contrib.auth import get_user_model
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = get_user_model()
--- 08-02-authentication/accounts/views.py
+++ 08-02-authentication/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 # Create your views here.
