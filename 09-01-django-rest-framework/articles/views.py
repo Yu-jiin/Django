6d6cd86c89d41d8e67f2ec9344d8cb5389bc4581
Yujiin <jiin4083@naver.com>
2024-10-16 10:23:49 +0900

    지금은 template 안만든다

--- 09-01-django-rest-framework/articles/views.py
+++ 09-01-django-rest-framework/articles/views.py
@@ -1,8 +1,14 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+from .models import Article
+from .serializer import ArticleListSerializer
 
-
+@api_view(['GET'])
 def article_list(request):
-    pass
+    articles = Article.objects.all()
+    serializer = ArticleListSerializer(articles, many=True)
+    return Response(serializer.data)
 
 
 def article_detail(request, article_pk):
