2880f9552378dd90d3f7fbbc61ccbb07520d0803
Yujiin <jiin4083@naver.com>
2024-10-17 10:41:58 +0900

    open API 페이지 done

--- 09-02-django-rest-framework/drf/urls.py
+++ 09-02-django-rest-framework/drf/urls.py
@@ -22,3 +22,12 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/v1/', include('articles.urls')),
 ]
+
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+urlpatterns = [
+    # YOUR PATTERNS
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+]
